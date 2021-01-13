1a2ba87906123bec90fb151844d756cb07977d7a
Abdallah El Hanafi <abdallah.elhanafi@learnet.se>
2021-01-13 14:38:41 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,42 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'mars',
+    loadChildren: () => import('./pages/mars/mars.module').then( m => m.MarsPageModule)
+  },
+  {
+    path: 'merkurius',
+    loadChildren: () => import('./pages/merkurius/merkurius.module').then( m => m.MerkuriusPageModule)
+  },
+  {
+    path: 'pluto',
+    loadChildren: () => import('./pages/pluto/pluto.module').then( m => m.PlutoPageModule)
+  },
+  {
+    path: 'tellus',
+    loadChildren: () => import('./pages/tellus/tellus.module').then( m => m.TellusPageModule)
+  },
+  {
+    path: 'neptunus',
+    loadChildren: () => import('./pages/neptunus/neptunus.module').then( m => m.NeptunusPageModule)
+  },
+  {
+    path: 'venus',
+    loadChildren: () => import('./pages/venus/venus.module').then( m => m.VenusPageModule)
+  },
+  {
+    path: 'uranus',
+    loadChildren: () => import('./page/uranus/uranus.module').then( m => m.UranusPageModule)
+  },
+  {
+    path: 'saturnus',
+    loadChildren: () => import('./page/saturnus/saturnus.module').then( m => m.SaturnusPageModule)
+  },
+  {
+    path: 'jupiter',
+    loadChildren: () => import('./page/jupiter/jupiter.module').then( m => m.JupiterPageModule)
+  },
 ];
 
 @NgModule({
